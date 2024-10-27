44515bf726c5f86c639d50b8acaabfce160eef36
areynot <alejandro.reynot@arivo.com>
2024-10-27 18:47:22 -0300

    main

--- app/core/models.py
+++ app/core/models.py
@@ -9,11 +9,15 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
 
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
